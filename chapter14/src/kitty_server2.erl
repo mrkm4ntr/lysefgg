7f32f2e583c13513e9aa7a7ad1c976599ea6b28b
Shintaro Murakami <mrkm4ntr@gmail.com>
2017-06-15 00:50:12 +0900

    More abstraction

--- chapter14/src/kitty_server2.erl
+++ chapter14/src/kitty_server2.erl
@@ -1,46 +1,43 @@
 -module(kitty_server2).
--compile(export_all).
+-export([start_link/0, order_cat/4, return_cat/2, close_shop/1]).
+-export([init/1, handle_call/3, handle_cast/2]).
 
 -record(cat, {name, color=green, description}).
 
 %%% Client API
-start_link() -> spawn_link(?MODULE, init, []).
+start_link() -> my_server:start_link(?MODULE, []).
 
 order_cat(Pid, Name, Color, Description) ->
   my_server:call(Pid, {order, Name, Color, Description}).
 
 return_cat(Pid, Cat = #cat{}) ->
-  Pid ! {return, Cat},
-  ok.
+  my_server:cast(Pid, {return, Cat}).
 
 close_shop(Pid) ->
   my_server:call(Pid, terminate).
 
-%%% Server functions
-init() -> loop([]).
+%%% Callback functions
+init([]) -> [].
 
-loop(Cats) ->
-  receive
-    {Pid, Ref, {order, Name, Color, Description}} ->
-      if Cats =:= [] ->
-          Pid ! {Ref, make_cat(Name, Color, Description)},
-          loop(Cats);
-         Cats =/= [] ->
-           Pid ! {Ref, hd(Cats)},
-           loop(tl(Cats))
-      end;
-    {return, Cat = #cat{}} ->
-      loop([Cat | Cats]);
-    {Pid, Ref, terminate} ->
-      Pid ! {Ref, ok},
-      terminate(Cats);
-    Unknown ->
-      io:format("Unknown message: ~p~n", [Unknown]),
-      loop(Cats)
-  end.
+handle_call({order, Name, Color, Description}, Hoge, Cats) ->
+  if Cats =:= [] ->
+       my_server:reply(Hoge, make_cat(Name, Color, Description)),
+       Cats;
+     Cats =/= [] ->
+       my_server:reply(Hoge, hd(Cats)),
+       tl(Cats)
+  end;
 
+handle_call(terminate, Hoge, Cats) ->
+  myserver:reply(Hoge, ok),
+  terminate(Cats).
+
+handle_cast({return, Cat=#cat{}}, Cats) ->
+  [Cat | Cats].
+
+%%% Private functions
 make_cat(Name, Color, Description) -> #cat{name=Name, color=Color, description=Description}.
 
 terminate(Cats) ->
   [io:format("~p was set free.~n", [C#cat.name]) || C <- Cats],
-  ok.
+  exit(normal).
