92447145d0a0f846dd24da272e32a151930933b5
Shintaro Murakami <mrkm4ntr@gmail.com>
2017-06-19 19:57:53 +0900

    Add curling_feed

--- chapter16/src/curling.erl
+++ chapter16/src/curling.erl
@@ -1,5 +1,5 @@
 -module(curling).
--export([start_link/2, set_teams/2, add_points/2, next_round/0]).
+-export([start_link/2, set_teams/2, add_points/2, next_round/0, join_feed/1, leave_feed/1]).
 
 start_link(TeamA, TeamB) ->
   {ok, Pid} = gen_event:start_link({local, ?MODULE}),
@@ -14,4 +14,12 @@ add_points(Team, N) ->
   gen_event:notify(?MODULE, {add_points, Team, N}).
 
 next_round() ->
-  gen_even:notify(next_round).
+  gen_event:notify(?MODULE, next_round).
+
+join_feed(Pid) ->
+  HandlerId = {curling_feed, make_ref()},
+  gen_event:add_handler(?MODULE, HandlerId, [Pid]),
+  HandlerId.
+
+leave_feed(HandlerId) ->
+  gen_event:delete_handler(?MODULE, HandlerId, leave_feed).
